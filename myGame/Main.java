9003d4fbbb571e6cff3307d2ce58177130ab6964
Ekaterina <mishelledale@gmail.com>
2022-12-01 22:07:01 +0300

    Directory added

---
+++ myGame/Main.java
@@ -0,0 +1,94 @@
+package myGame;
+import System.ConsoleView;
+import charts.Peasant;
+import charts.Unit;
+import charts.darkSide.Xbowman;
+import charts.darkSide.Monk;
+import charts.darkSide.Spearman;
+import charts.whiteSide.Robber;
+import charts.whiteSide.Sniper;
+import charts.whiteSide.Wizard;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main{
+    private static final int GANG_SIZE = 10;
+    private static final int FIELD_WIDTH = 10;
+    public static List<Unit> blueSide;
+    public static List<Unit> greenSide;
+
+    public static void main(String[] args) {
+        runGames();
+
+        Scanner scanner = new Scanner(System.in); 
+        
+        while (true) {
+            ConsoleView.view();
+            turnMove();
+            scanner.nextLine();
+            
+        }
+      
+    }
+    private static void turnMove() {
+        List<Unit> sortSide = new ArrayList<>();
+        sortSide.addAll(blueSide);
+        sortSide.addAll(greenSide);
+        sortSide.sort(new Comparator<Unit>() {
+            @Override
+            public int compare(Unit o1, Unit o2) {
+                int tmp = o2.getSpeed() - o1.getSpeed();
+                return tmp;
+            }
+        });
+        sortSide.forEach(Unit::step);
+    }
+
+    private static void runGames() {
+
+        blueSide = new ArrayList<>();
+        greenSide = new ArrayList<>();
+
+        int x = 1;
+        int y = 1;
+        for (int i = 0; i < GANG_SIZE; i++) {
+            switch (new Random().nextInt(4)) {
+                case 0 -> blueSide.add(new Robber(blueSide, greenSide, x, y++));
+                case 1 -> blueSide.add(new Sniper(blueSide, greenSide, x, y++));
+                case 2 -> blueSide.add(new Wizard(blueSide, greenSide, x, y++));
+                default -> blueSide.add(new Peasant(blueSide, greenSide, x, y++));
+            }
+        }
+
+        x = FIELD_WIDTH;
+        y = 1;
+        for (int i = 0; i < GANG_SIZE; i++) {
+            switch (new Random().nextInt(4)) {
+                case 0 -> greenSide.add(new Peasant(greenSide, blueSide, x, y++));
+                case 1 -> greenSide.add(new Spearman(greenSide, blueSide, x, y++));
+                case 2 -> greenSide.add(new Xbowman(greenSide, blueSide, x, y++));
+                default -> greenSide.add(new Monk(greenSide, blueSide, x, y++));
+            }
+        }
+    }
+    public static int getGangSize() {
+        return GANG_SIZE;
+    }
+
+    public static int getFieldWidth() {
+        return FIELD_WIDTH;
+    }
+
+    public static List<Unit> getBlueSide() {
+        return blueSide;
+    }
+
+    public static List<Unit> getGreenSide() {
+        return greenSide;
+    }
+}
