8a71afec9a9e887ca0c61caf440c2422810ee291
Ekaterina <mishelledale@gmail.com>
2022-12-06 18:45:46 +0300

    Step added for a robber to go and random quantity added

--- charts/darkSide/Spearman.java
+++ charts/darkSide/Spearman.java
@@ -1,6 +1,8 @@
 package charts.darkSide;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import System.Vector2;
 import charts.Unit;
@@ -11,7 +13,58 @@ public class Spearman extends Unit{
         super.gang = gang;
         super.position = new Vector2(x, y);
         super.side = side;
+        quantity = new Random().nextInt(10,15);
     }   
+
+    @Override
+    public void step(){
+        double dist = Double.MAX_VALUE;
+        int index = -1;
+        for (int i = 0; i <side.size(); i++) {
+            if (side.get(i).action.equals("Dead")) {
+                continue;
+            }
+
+            double tmp = side.get(i).getPosition().getDist(this.getPosition());
+            if (dist > tmp) {
+                dist = tmp;
+                index = i;
+            }
+        }
+        if (index >= 0) {
+            if (dist < 2){
+                side.get(index).getHit(calcDamage(side.get(index)));
+            } else {
+                Vector2 enemyPos = side.get(index).getPosition();
+                Vector2 newPos = new Vector2(0, 0);
+                if (enemyPos.y == position.y) {
+                    newPos.y = position.y;
+                    if (position.x - enemyPos.x < 0) {
+                        newPos.x = position.x+1;
+                    } else {
+                        newPos.x = position.x-1;
+                    }
+                } else{
+                    newPos.x = position.x;
+                    if (enemyPos.y - position.y > 0){
+                        newPos.y = position.y + 1;
+                    } else {
+                        newPos.y = position.y - 1;
+                    }
+                }
+                AtomicBoolean empty = new AtomicBoolean(true);
+                gang.forEach(unit -> {
+                    if (unit.getPosition().isEquals(newPos)) {
+                        empty.set(false);;
+                    }
+                });
+                if (empty.get()) {
+                    position = newPos;
+                }
+            }
+        }
+    }
+
     
     @Override 
     public String getInfo() {
