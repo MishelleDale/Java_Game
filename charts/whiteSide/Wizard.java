a304fd37ab92057b792795e8adb46eb59785eea8
Ekaterina <mishelledale@gmail.com>
2022-12-06 18:46:18 +0300

    Random qauntity added

--- charts/whiteSide/Wizard.java
+++ charts/whiteSide/Wizard.java
@@ -1,6 +1,6 @@
 package charts.whiteSide;
 import java.util.List;
-
+import java.util.Random;
 import System.Vector2;
 import charts.Unit;
 
@@ -11,6 +11,7 @@ public class Wizard extends Unit{
         super.gang = gang;
         super.position = new Vector2(x, y);
         super.side = side;
+        quantity = new Random().nextInt(1,4);
     }
 
     @Override 
@@ -18,24 +19,23 @@ public class Wizard extends Unit{
         return "Wizard " + super.getInfo();
     }
 
-    // @Override
-    // public void step(){
-    //     float minHealth = Integer.MAX_VALUE;
-    //     int minIndex = -1;
-    //     for (int i = 0; i < gang.size(); i++) {
-    //         if (gang.get(i).health < gang.get(i).maxHealth){
-    //             if (gang.get(i).health < minHealth) {
-    //                 minHealth = gang.get(i).health;
-    //                 minIndex = i;
-    //             }
-    //         }
-    //     }
-    //     if (minHealth >= 0) {
-    //         gang.get(minIndex).health -= this.damage[0];
-    //         if (gang.get(minIndex).health > gang.get(minIndex).maxHealth){
-    //             gang.get(minIndex).health = gang.get(minIndex).maxHealth;
-    //         }
-    //     }
-    // }   
-    
+    @Override
+    public void step(){
+        double minHealth = Integer.MAX_VALUE;
+        int minIndex = -1;
+        for (int i = 0; i < gang.size(); i++) {
+            if (gang.get(i).health > 0 && gang.get(i).health < gang.get(i).maxHealth) {
+                if (gang.get(i).health < minHealth) {
+                    minHealth = gang.get(i).health;
+                    minIndex = i;
+                }
+            }
+        }
+        if (minIndex >= 0) {
+            gang.get(minIndex).health -= this.damage[0];
+            if (gang.get(minIndex).health > gang.get(minIndex).maxHealth) {
+                gang.get(minIndex).health = gang.get(minIndex).maxHealth;
+            }
+        }
+    }     
 }
