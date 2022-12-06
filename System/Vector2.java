ed6f89f53a4ad5d23ca760d9a8e2c01cc3a320ab
Ekaterina <mishelledale@gmail.com>
2022-12-06 18:40:52 +0300

    Some lines under comment

--- System/Vector2.java
+++ System/Vector2.java
@@ -1,34 +1,41 @@
 package System;
 
 public class Vector2 {
-    int x, y;
+    public int x;
+    public int y;
 
     public Vector2(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public int getX() {
-        return x;
-    }
+    // public int getX() {
+    //     return x;
+    // }
 
-    public void setX(int x) {
-        this.x = x;
-    }
+    // public void setX(int x) {
+    //     this.x = x;
+    // }
 
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
+    // public void setY(int y) {
+    //     this.y = y;
+    // }
 
     public boolean isEquals(Vector2 opposit) {
-        return opposit.y == y && opposit.x == x;
+        if (opposit.y == y && opposit.x == x) return true;
+        return false;
     }
 
     public double getDist(Vector2 opposit) {
         return Math.sqrt(Math.pow(opposit.x - this.x, 2) + Math.pow(opposit.y - this.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "x: " + x + ", y: " + y;
     }
 }
