366720f1076c57804a85e83bb5bf5fbd33348940
Ekaterina <mishelledale@gmail.com>
2022-12-09 17:31:51 +0300

    Step rewrite

--- charts/darkSide/Monk.java
+++ charts/darkSide/Monk.java
@@ -1,11 +1,17 @@
 package charts.darkSide;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
-
 import System.Vector2;
 import charts.Unit;
 
 public class Monk extends Unit{
+
+    static int tmpInt = -1;
+
     public Monk(List<Unit> gang,  List<Unit> side, int x, int y) {
         super(12, 7, 0, new int[] {-4, -4}, 30, 5, false, true, "Monk");
         super.gang = gang;
@@ -21,21 +27,60 @@ public class Monk extends Unit{
 
     @Override
     public void step(){
-        double minHealth = Integer.MAX_VALUE;
-        int minIndex = -1;
+        Map<Integer, Double> healths = new HashMap<>();
         for (int i = 0; i < gang.size(); i++) {
-            if (gang.get(i).health > 0 && gang.get(i).health < gang.get(i).maxHealth) {
-                if (gang.get(i).health < minHealth) {
-                    minHealth = gang.get(i).health;
-                    minIndex = i;
+            healths.put(i, gang.get(i).health / gang.get(i).maxHealth);
+        }
+        List<Double> a = new ArrayList<>(healths.values().stream().toList());
+        Collections.sort(a);
+        healths.forEach((index, value) -> {
+            if (value.equals(a.get(0))){
+                tmpInt = index;
+            }
+        });
+
+        if(a.get(0) > 0.5) {
+            double dist = 1;
+            int index = -1;
+            for (int i = 0; i < side.size(); i++) {
+                if (side.get(i).action.equals("Dead")) {
+                    continue;
+                }
+                if (side.get(i).health / side.get(i).maxHealth < dist){
+                    dist = side.get(i).health / side.get(i).maxHealth;
+                    index = 1;
                 }
             }
-        }
-        if (minIndex >= 0) {
-            gang.get(minIndex).health -= this.damage[0];
-            if (gang.get(minIndex).health > gang.get(minIndex).maxHealth) {
-                gang.get(minIndex).health = gang.get(minIndex).maxHealth;
+            if (index < 0){
+                index = 0;
             }
+            side.get(index).getHit(damage[0] * -1);
+            action = "Shooted into " + index;
+            return;            
+        }
+        if (a.get(0).equals(0.0)){
+            tmpInt = -1;
+            healths.forEach((index, value) -> {
+                if (value.equals(0.0)) {
+                    if (gang.get(index).getName().equals("Xbowman") || gang.get(index).getName().equals("Monk")){
+                        tmpInt = index;
+                    }
+                }
+            });
+            if (tmpInt >= 0){
+                gang.get(tmpInt).health = 1;
+                gang.get(tmpInt).action = "Stand";
+                gang.get(tmpInt).quantity = 1;
+                action = "Revived " + tmpInt;
+            }
+            return;
+        }
+        if (a.get(0) <= 0.5) {
+            gang.get(tmpInt).health -= this.damage[0];
+            if (gang.get(tmpInt).health > gang.get(tmpInt).maxHealth) {
+                gang.get(tmpInt).health = gang.get(tmpInt).maxHealth;
+            }
+            action = "Treated " + tmpInt;
         }
     } 
 }
